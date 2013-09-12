f8d4547f20ccc9da90945e40483839604294b6f2
jens <jens.brimberg@gmail.com>
2013-09-12 09:50:44 +0200

    work

--- src/main/java/se/jensim/chargen/ui/WelcomeUI.java
+++ src/main/java/se/jensim/chargen/ui/WelcomeUI.java
@@ -4,12 +4,14 @@ import com.vaadin.cdi.CDIUI;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @CDIUI("")
-public class WelcomeUI extends UI {
+public class WelcomeUI extends UI implements Button.ClickListener{
 
 	private static final long serialVersionUID = 1511280419027917279L;
 
@@ -17,15 +19,15 @@ public class WelcomeUI extends UI {
 	protected void init(VaadinRequest request) {
 		// TODO Auto-generated method stub
 		Label lbl = new Label("", ContentMode.HTML);
-		Button btn = new Button("BUTTON");//, this);
+		Button btn = new Button("BUTTON", this);
 		VerticalLayout layout = new VerticalLayout(lbl, btn);
 		setContent(layout);
 	}
-/*
+
 	@Override
 	public void buttonClick(ClickEvent event) {
 		Notification.show("Button was clicked. Good Work!...");
 
 	}
-*/
+
 }
